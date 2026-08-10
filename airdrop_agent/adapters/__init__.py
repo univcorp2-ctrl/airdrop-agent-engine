1195d9ea0ebbfebf5fbabcd24d8ef1c9bd139132
univcorp2-ctrl <univcorp2@gmail.com>
2026-08-10 21:31:41 +0900

    Build scheduled DRY_RUN airdrop agent engine

---
+++ airdrop_agent/adapters/__init__.py
@@ -0,0 +1,6 @@
+from .hibachi import HibachiAdapter
+from .kyan import KyanAdapter
+from .lighter import LighterAdapter
+from .pacifica import PacificaAdapter
+
+__all__ = ["PacificaAdapter", "HibachiAdapter", "KyanAdapter", "LighterAdapter"]
